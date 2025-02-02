9ba00040b50dcbc260cf6746d0f34c3d62b8bd1a
Kelwoi <vova0110gordienko@gmail.com>
2025-02-02 23:42:33 +0200

    Moder

---
+++ Moderation/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+
+
+            string moderationFilePath = "../../../moderation_words.txt";
+            string textFilePath = "../../../text.txt";
+
+
+
+            if (!File.Exists(moderationFilePath))
+            {
+                Console.WriteLine("file with words for moderation does not exist.");
+                return;
+            }
+
+            if (!File.Exists(textFilePath))
+            {
+                Console.WriteLine("file does not exist.");
+                return;
+            }
+
+            string[] moderationWords = File.ReadAllLines(moderationFilePath);
+            string[] textLines = File.ReadAllLines(textFilePath);
+
+
+            for (int i = 0; i < textLines.Length; i++)
+            {
+                foreach (var word in moderationWords)
+                {
+                    string mask = new string('*', word.Length);
+                    textLines[i] = textLines[i].Replace(word, mask);
+                }
+            }
+
+
+            string outputFilePath = "../../../moderated_text.txt";
+            File.WriteAllLines(outputFilePath, textLines);
+
+            Console.WriteLine($"Moderation is finished in file: {outputFilePath}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"error: {ex.Message}");
+        }
+    }
+}
