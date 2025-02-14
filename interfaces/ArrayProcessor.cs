450fff2deac41fd7596443caf2b7d5a828648c46
Kelwoi <vova0110gordienko@gmail.com>
2025-02-14 15:00:07 +0200

    interfaces

---
+++ interfaces/ArrayProcessor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class ArrayProcessor
+    {
+        
+    private int[] array;
+
+        public ArrayProcessor(int[] arr)
+        {
+            array = arr;
+        }
+
+
+        public int Less(int valueToCompare)
+        {
+            return array.Count(x => x < valueToCompare);
+        }
+
+        public int Greater(int valueToCompare)
+        {
+            return array.Count(x => x > valueToCompare);
+        }
+
+
+        public void ShowEven()
+        {
+            Console.WriteLine("Even numbers: " + string.Join(", ", array.Where(x => x % 2 == 0)));
+        }
+
+        public void ShowOdd()
+        {
+            Console.WriteLine("Odd numbers: " + string.Join(", ", array.Where(x => x % 2 != 0)));
+        }
+
+
+        public int CountDistinct()
+        {
+            return array.Distinct().Count();
+        }
+
+        public int EqualToValue(int valueToCompare)
+        {
+            return array.Count(x => x == valueToCompare);
+        }
+    }
+}
---
+++ interfaces/Program.cs
@@ -0,0 +1,19 @@
+﻿using Interfaces;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5, 3, 2, 1, 6, 7, 8, 8, 9 };
+        ArrayProcessor processor = new ArrayProcessor(numbers);
+
+        Console.WriteLine("Less than 5: " + processor.Less(5));
+        Console.WriteLine("Greater than 5: " + processor.Greater(5));
+
+        processor.ShowEven();
+        processor.ShowOdd();
+
+        Console.WriteLine("Distinct count: " + processor.CountDistinct());
+        Console.WriteLine("Count of 3: " + processor.EqualToValue(3));
+    }
+}
