225b6e70aed8fdc1443666c9784f51ad992578a4
Kelwoi <vova0110gordienko@gmail.com>
2025-04-09 16:09:22 +0300

    Fibo

---
+++ Fibo_count/FibonacciCounter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibo_count
+{
+    internal class FibonacciCounter
+    {
+        private int n1 = 0;
+        private int n2 = 1;
+        private int evenCount = 0;
+
+        private readonly object locker = new();
+
+        public void CalculateNext(string threadName)
+        {
+            lock (locker)
+            {
+                int next = n1 + n2;
+
+                if (next % 2 == 0)
+                    evenCount++;
+
+                n1 = n2;
+                n2 = next;
+
+                Console.WriteLine($"[{threadName}] n1: {n1}, n2: {n2}, evenCount: {evenCount}");
+            }
+        }
+
+    }
+}
---
+++ Fibo_count/Program.cs
@@ -0,0 +1,35 @@
+﻿using Fibo_count;
+using System.Diagnostics.Metrics;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var counter = new FibonacciCounter();
+
+        Thread thread1 = new Thread(() => Run(counter, "Thread-1"));
+        Thread thread2 = new Thread(() => Run(counter, "Thread-2"));
+        Thread thread3 = new Thread(() => Run(counter, "Thread-3"));
+
+        Console.WriteLine("press enter button to start...");
+        Console.ReadLine();
+
+        thread1.Start();
+        thread2.Start();
+        thread3.Start();
+
+        thread1.Join();
+        thread2.Join();
+        thread3.Join();
+
+        Console.WriteLine("Ready!");
+    }
+    static void Run(FibonacciCounter counter, string name)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            counter.CalculateNext(name);
+            Thread.Sleep(50);
+        }
+    }
+    
+}
