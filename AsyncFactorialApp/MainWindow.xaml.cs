5e1c05d9f55a3ad4da78930774ad6821b3a21829
Kelwoi <vova0110gordienko@gmail.com>
2025-04-11 17:42:53 +0300

    factor

---
+++ AsyncFactorialApp/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AsyncFactorialApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(InputTextBox.Text, out int number) && number >= 0)
+            {
+                BigInteger factorial = await CalculateFactorialAsync(number);
+                ResultsListBox.Items.Add($"Factorial of {number} = {factorial}");
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid non-negative integer.", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private async Task<BigInteger> CalculateFactorialAsync(int number)
+        {
+            await Task.Delay(100);
+            BigInteger result = 1;
+            for (int i = 2; i <= number; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
