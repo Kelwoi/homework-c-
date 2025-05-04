39723f4b31e46c1e3b678314918ff645eb8436bf
Kelwoi <vova0110gordienko@gmail.com>
2025-05-05 01:29:31 +0300

    smtp

---
+++ SMTP_sender/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Win32;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace SMTP_sender
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const string server = "smtp.gmail.com";
+        const short port = 587;
+        string username = "";
+        string password = "";
+        private List<string> selectedFilePaths = new List<string>();
+
+
+        public MainWindow(string email, string Password)
+        {
+            InitializeComponent();
+
+            username = email;
+            password = Password;
+        }
+
+        private void SelectFile(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                selectedFilePaths.Clear();
+                listFiles.Items.Clear();
+
+                foreach (string file in openFileDialog.FileNames)
+                {
+                    selectedFilePaths.Add(file);
+                    listFiles.Items.Add(System.IO.Path.GetFileName(file));
+                }
+            }
+        }
+
+        private string GetRichText(RichTextBox richTextBox)
+        {
+            TextRange textRange = new TextRange(
+                richTextBox.Document.ContentStart,
+                richTextBox.Document.ContentEnd
+            );
+            return textRange.Text;
+        }
+
+
+        private void SendMessage(object sender, RoutedEventArgs e)
+        {
+            MailMessage message = new MailMessage(username, toBox.Text, themeBox.Text, GetRichText(messageBox));
+
+
+
+            message.Priority = MailPriority.Low;
+            if (MediumImportance.IsChecked == true)
+                message.Priority = MailPriority.Normal;
+            else if (HighImportance.IsChecked == true)
+                message.Priority = MailPriority.High;
+
+            SmtpClient client = new SmtpClient(server, port);
+
+            client.Credentials = new NetworkCredential(username, password);
+            client.EnableSsl = true;
+
+            client.SendAsync(message, new object());
+
+            client.SendCompleted += SendMessageCompleted;
+
+        }
+
+        private void SendMessageCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("Sent succesfully!");
+        }
+    }
+}
---
+++ SMTP_sender/Window1.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SMTP_sender
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private bool _isPasswordVisible = false;
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            string email = EmailBox.Text;
+            string password = PasswordBox.Password;
+
+            var mainWindow = new MainWindow(email, password);
+            mainWindow.Show();
+            this.Close(); 
+        }
+        private void TogglePasswordVisibility_Click(object sender, RoutedEventArgs e)
+        {
+            _isPasswordVisible = !_isPasswordVisible;
+
+            if (_isPasswordVisible)
+            {
+                VisiblePasswordBox.Text = PasswordBox.Password;
+                VisiblePasswordBox.Visibility = Visibility.Visible;
+                PasswordBox.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                PasswordBox.Password = VisiblePasswordBox.Text;
+                PasswordBox.Visibility = Visibility.Visible;
+                VisiblePasswordBox.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
